dce943943050e53bbb126309edf4cba3e6ae4d9c
Rick <ricksmycodeguy@gmail.com>
2023-04-09 14:33:25 -0500

    Applies useCallback ensures setShowParagraph isn't rerendered unncessarily via memoization

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Button from './components/UI/Button/Button';
 import './App.css';
 import DemoOutput from './components/UI/Button/Demo/DemoOutput';
 
 function App() {
-  console.log('APP RUNNING');
   const [showParagraph, setShowParagraph] = useState(false);
-  const toggleParagraphHandler = () => { 
+
+  console.log('APP RUNNING');
+
+  const toggleParagraphHandler = useCallback(() => { 
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
     //could be written as setShowParagraph(!showParagraph);
-  };
+  }, []);
+  
   return (
     <div className="app">
       <h1>Hi there!</h1>
